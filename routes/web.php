dacc49f81523440f0413f8b86b5be8fddec9a759
Vincent Desrosiers <vincentdesrd@gmail.com>
2019-05-19 11:27:51 -0400

    Ménage dans les routes

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,9 @@ Route::get('/search', 'RechercheController@search');
 Route::get('q', 'AutoCompleteController@index');
 Route::get('autocomplete', 'AutoCompleteController@search');
 Route::name('user')->get('user/{user}', 'ImageController@user');
-/* Route::resource('profile', 'ProfileController'); */
 Route::resource('profile', 'ProfileController', [
-        'only' => ['index', 'edit', 'update', 'destroy', 'show'],
         'parameters' => ['profile' => 'user'],
     ]);
-/* Route::resource('user', 'UserController'); */
 Route::resource('user', 'UserController', [
-    'only' => ['index', 'edit', 'update', 'destroy', 'show'],
     'parameters' => ['user' => 'user'],
     ]);
